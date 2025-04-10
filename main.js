2e215fb7df424f13ddcd9ac0627a97465d14a536
Carlos Eduardo Salles de Sousa <carloseduardo.rss@gmail.com>
2025-04-10 12:14:09 -0300

    change: Remove funções de main.js já presentes em form.js e adiciona classe .campo-senha aos estilos

--- main.js
+++ main.js
@@ -31,23 +31,6 @@ let telefoneMask = IMask(telefone, {
 let cepMask = IMask(cep, {mask: "00000-000"});
 let numeroCasaMask = IMask(numeroCasa, {mask: Number, scale: 0})
 
-function obterAlertaSpan(input) {
-    return input.parentElement.querySelector(".alerta") ?? input.parentElement.parentElement.querySelector(".alerta");
-}
-function exibirErro(input, mensagem) {
-    const alerta = obterAlertaSpan(input);
-    alerta.textContent = mensagem;
-
-    input.classList.add("campo-padrao--invalido");
-    alerta.classList.remove("alerta--escondido");
-}
-function recolherErro(input) {
-    const alerta = obterAlertaSpan(input);
-    alerta.textContent = "";
-
-    input.classList.remove("campo-padrao--invalido");
-    alerta.classList.add("alerta--escondido");
-}
 function cpfValido(cpfDigitosString) {
     if (!cpfDigitosString.match(/\d{11}/)) return false;
 
@@ -187,22 +170,6 @@ function validarTrilhaDeAprendizagem() {
         recolherErro(trilhaDeAprendizagem);
     }
 }
-function validarUsuarioId() {
-    if (usuarioId.validity.valueMissing) {
-        exibirErro(usuarioId, "Campo obrigatório");
-    }
-    else {
-        recolherErro(usuarioId);
-    }
-}
-function validarUsuarioSenha() {
-    if (usuarioSenha.validity.valueMissing) {
-        exibirErro(usuarioSenha, "Campo obrigatório");
-    }
-    else {
-        recolherErro(usuarioSenha);
-    }
-}
 function validarConfirmacaoSenhas() {
     if (usuarioSenha.value != usuarioSenhaConfirm.value) {
         exibirErro(usuarioSenhaConfirm, "Senhas diferem")
@@ -248,51 +215,6 @@ function preencherCamposCep() {
 }
 
 const formStorageItem = "formulario-inscricao-rascunho";
-const formStorage = {
-    load(item) {
-        let dados = localStorage.getItem(item);
-        if (dados === null) return;
-        
-        dados = JSON.parse(dados);
-        for (const name in dados) {
-            const {type, value} = dados[name];
-            const elements = Array.from(document.getElementsByName(name));
-
-            if (type == "checkbox") {
-                elements.forEach(el => {el.checked = value});
-            }
-            else if (type == "radio") {
-                elements.forEach(el => {el.checked = (el.value == value)})
-            }
-            else {
-                elements.forEach(el => el.value = value);
-            }
-        }
-    },
-    save(item, elements) {
-        const dados = {};
-        for (const el of elements) {
-            let type, value;
-            if (el?.type == "checkbox") {
-                type = "checkbox";
-                value = el.checked;
-            }
-            else if (el?.type == "radio") {
-                const inputChecked = document.querySelector(`[name="${el.name}"]:checked`);
-                type = "radio";
-                value = inputChecked ? inputChecked.value : "";
-            }
-            else {
-                type = "default",
-                value = el.value;
-            }
-            dados[el.name] = {type, value};
-        }
-        
-        localStorage.setItem(item, JSON.stringify(dados));
-    }
-}
-
 
 function carregarRascunho() {
     formStorage.load(formStorageItem);
